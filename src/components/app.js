f2ac75e74eeb8c4dece5cc161e8d8f0c378e8e8c
Doug Modie <dmodie@gmail.com>
2016-07-08 15:30:15 -0500

    book list does something

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 
 import React, { Component } from 'react';
-import BookList from './book-list';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter
+      <div>
         <BookList />
       </div>
     );
--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -3,13 +3,22 @@ class BooksList extends Component {
     constructor(props){
         super(props);
         this.list = ["A Farewell to Arms", "A Tale of Two Cities"]
+        console.log ("hello")
 
     }
-    
+
+    renderList(){
+      return this.list.map((book) => {
+        return (
+          <li key={book} className="list-group-item">{book}</li>
+        )
+      })
+    }
+
     render (){
       return (
-          <ul className="books-list" >
-            {this.list}
+          <ul className="list-group col-sm-4" >
+            {this.renderList()}
           </ul>
 
       );
